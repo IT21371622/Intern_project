42cf5295e58fcb61b95103f24cb566667aa53644
IT21371622 <it21371622@my.sliit.lk>
2023-06-25 10:09:02 +0530

    commit

---
+++ employee/models/employee.js
@@ -0,0 +1,32 @@
+const mongoose = require('mongoose');
+
+const EmployeeSchema = new mongoose.Schema({
+    firstName: {
+        type: String,
+        
+    },
+    lastName: {
+        type: String,
+        
+    },
+    email: {
+        type: String,
+       
+    },
+
+   
+    gender: {
+        type: String,
+       
+    },
+    contactNo: {
+        type: Number,
+       
+    },
+
+    
+});
+
+
+
+module.exports = mongoose.model('EmployeeTable', EmployeeSchema);
